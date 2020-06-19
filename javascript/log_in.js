a0a389f0d9cd5e3c47f8622c872fbb9c79b7cc2a
Danyal Jamil <danyal.jcc@gmail.com>
2020-06-19 05:16:47 +0300

    Project Files

---
+++ javascript/log_in.js
@@ -0,0 +1,56 @@
+// var bool = true;
+
+// var email = document.forms['form']['email'];
+
+// var psw = document.forms['form']['psw'];
+
+// var email_error = document.getElementById('email_error');
+// var psw_error = document.getElementById('psw_error');
+
+// email.addEventListener('textInput', email_Verify);
+// psw.addEventListener('textInput', psw_Verify);
+
+// function validated() {
+// 	if (email.value.length < 0) {
+// 		email.style.border = "1px solid red";
+
+// 		email_error.style.display = 'block';
+// 		email.focus();
+// 		bool = false;
+// 		return false;
+// 	}
+
+// 	if (psw.value.length < 6) {
+// 		psw.style.border = "1px solid red";
+
+// 		psw_error.style.display = 'block';
+// 		psw.focus();
+// 		bool = false;
+// 		return false;
+// 	}
+// }
+
+// function email_Verify() {
+// 	if (email.value.length >= 8) {
+// 		email.style.border = "1px solid silver";
+
+// 		email_error.style.display = 'none';
+// 		return true;
+// 	}
+// }
+
+// function psw_Verify() {
+// 	if (psw.value.length >= 6) {
+// 		psw.style.border = "1px solid silver";
+
+// 		psw_error.style.display = 'none';
+// 		return true;
+// 	}
+// }
+
+// function check() {
+// 	//if () {
+// 		alert();
+// 		window.location.href = "http://www.w3schools.com";
+// 	//}
+// }
